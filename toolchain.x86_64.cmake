cefa6b854e9a78da0c8c1d518ec4ab80fb0b99de
mbea1 <>
2024-04-09 02:26:09 +0000

    Container-based dev environment supporting x86_64/aarch64 and using CMake

---
+++ toolchain.x86_64.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME linux)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_C_COMPILER /toolchains/x86_64-glibc/bin/x86_64-linux-gcc)
+set(CMAKE_CXX_COMPILER /toolchains/x86_64-glibc/bin/x86_64-linux-g++)
+
+set(CMAKE_FIND_ROOT_PATH /toolchains/x86_64-glibc)
